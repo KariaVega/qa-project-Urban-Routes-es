e3e74dd3d7e11ec42bc1e4dd22759a42a7717513
Tlalpan@karinavega <arqkarvga@gmail.com>
2024-06-25 15:36:18 -0600

    Inicio de clonación

--- data.py
+++
@@ -1,6 +0,0 @@
-urban_routes_url = 'https://cnt-61558c1c-b26c-4804-a53e-0fe0919bc348.containerhub.tripleten-services.com?lng=es'
-address_from = 'East 2nd Street, 601'
-address_to = '1300 1st St'
-phone_number = '+1 123 123 12 12'
-card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'Muéstrame el camino al museo'
